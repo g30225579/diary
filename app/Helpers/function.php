563cf7ebb354e39d9b90b3f2a81df850f331a1f4
ximenchuihuo <ximenchuihuo@noreply.com>
2024-04-01 11:38:26 +0800

    upgrade package & fixed

--- app/Helpers/function.php
+++ app/Helpers/function.php
@@ -1,4 +1,6 @@
 <?php
+
+use Illuminate\Support\Str;
 use Jenssegers\Agent\Agent;
 
 // 是否移动浏览器（包含平板/手机）
@@ -10,4 +12,19 @@ function checkMobile()
 function checkPhone()
 {
     return (new Agent())->isPhone();
+}
+// 是否微信浏览器
+function checkWeChat()
+{
+    return Str::contains( strtolower(request()->server('HTTP_USER_AGENT')), 'micromessenger' );
+}
+// 是否IOS设备
+function checkIOS()
+{
+    return (new Agent())->is('iOS');
+}
+// 是否Android设备
+function checkAndroid()
+{
+    return (new Agent())->isAndroidOS();
 }
